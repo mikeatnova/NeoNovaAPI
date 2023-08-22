db2280684d8834e341562119ac45fd6e7beadc3f
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-08-22 12:08:03 -0400

    + added store hours to store model

--- NeoNovaAPI/Models/DbModels/Store.cs
+++ NeoNovaAPI/Models/DbModels/Store.cs
@@ -21,5 +21,19 @@ namespace NeoNovaAPI.Models.DbModels
         public double Longitude { get; set; }
         public double Latitude { get; set; }
         public string? Directions { get; set; }
+        public string? MondayOpeningTime { get; set; }
+        public string? MondayClosingTime { get; set; }
+        public string? TuesdayOpeningTime { get; set; }
+        public string? TuesdayClosingTime { get; set; }
+        public string? WednesdayOpeningTime { get; set; }
+        public string? WednesdayClosingTime { get; set; }
+        public string? ThursdayOpeningTime { get; set; }
+        public string? ThursdayClosingTime { get; set; }
+        public string? FridayOpeningTime { get; set; }
+        public string? FridayClosingTime { get; set; }
+        public string? SaturdayOpeningTime { get; set; }
+        public string? SaturdayClosingTime { get; set; }
+        public string? SundayOpeningTime { get; set; }
+        public string? SundayClosingTime { get; set; }
     }
 }
