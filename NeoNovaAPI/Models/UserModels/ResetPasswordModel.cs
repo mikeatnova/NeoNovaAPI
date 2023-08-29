ac9d0eec49f3004933a89d3e5a6e8250dd9be1a8
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-08-29 09:09:32 -0400

    + created resetpass model and reset code validation endpoint

---
+++ NeoNovaAPI/Models/UserModels/ResetPasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoNovaAPI.Models.UserModels
+{
+    public class ResetPasswordModel
+    {
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
