02d863cd41d910d6d2640660a9d40f851206cb0a
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-09-24 06:37:03 -0400

    + added Notes Model and edited other models to fit

---
+++ NeoNovaAPI/Models/SecurityModels/Reporting/Note.cs
@@ -0,0 +1,48 @@
+﻿using NeoNovaAPI.Models.SecurityModels.ShiftManagement;
+using NeoNovaAPI.Models.SecurityModels.TourManagement;
+using NeoNovaAPI.Models.UserModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeoNovaAPI.Models.SecurityModels.Reporting
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [ForeignKey("SecurityUser")]
+        public int UserId { get; set; }
+
+        public virtual SecurityUser User { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Role { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("Shift")]
+        public int? ShiftId { get; set; }
+
+        public virtual Shift Shift { get; set; }
+
+        [ForeignKey("Tour")]
+        public int? TourId { get; set; }
+
+        public virtual Tour Tour { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
