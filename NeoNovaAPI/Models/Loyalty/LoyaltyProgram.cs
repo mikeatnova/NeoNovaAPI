f4e3ab9d89cf22b3ea8ad68c1302a7f6e8371b34
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-10-07 10:35:56 -0400

    + bagan loyalty work, added seedSecurityUser endpoint

---
+++ NeoNovaAPI/Models/Loyalty/LoyaltyProgram.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeoNovaAPI.Models.Loyalty
+{
+    public class LoyaltyProgram
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastUpdated { get; set; }
+        public ICollection<LoyaltyTier> Tiers { get; set; } = new List<LoyaltyTier>();
+    }
+}
---
+++ NeoNovaAPI/Models/Loyalty/LoyaltyTier.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeoNovaAPI.Models.Loyalty
+{
+    public class LoyaltyTier
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int LoyaltyProgramId { get; set; }
+
+        [ForeignKey("LoyaltyProgramId")]
+        public LoyaltyProgram LoyaltyProgram { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? ImagePath { get; set; }
+        public string? BadgeImagePath { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<SmallPerk> SmallPerks { get; set; } = new List<SmallPerk>();
+        public ICollection<BigPerk> BigPerks { get; set; } = new List<BigPerk>();
+        [Required]
+        public int PointStart { get; set; }
+        [Required]
+        public int PointEnd { get; set; }
+        public string? Brand { get; set; }
+
+    }
+}
